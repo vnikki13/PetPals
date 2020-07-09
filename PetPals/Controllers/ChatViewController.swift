b36e0725f79a8097d20ebf50dc74912a94831b98
Nikki Vaughan <vnikki13@gmail.com>
2020-07-09 14:28:55 -0700

    created a chat view and controller

---
+++ PetPals/Controllers/ChatViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChatViewController.swift
+//  PetPals
+//
+//  Created by Nikki Vaughan on 7/9/20.
+//  Copyright © 2020 Nikki Vaughan. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "PetPals🐾"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PetPals/Controllers/HomeViewController.swift
+++ PetPals/Controllers/HomeViewController.swift
@@ -7,24 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
+        }
+          
     }
-    */
-
+    
 }
