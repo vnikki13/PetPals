bdd290d6be1722fbd5205ac0c3867e1948e556dc
Nikki Vaughan <vnikki13@gmail.com>
2020-07-09 13:53:20 -0700

    can register a new user and login existing users

--- PetPals/Controllers/LoginViewController.swift
+++ PetPals/Controllers/LoginViewController.swift
@@ -7,24 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    // TODO: create a label or popup for the user
+                    print(error.localizedDescription)
+                } else {
+                    print("success")
+                    self.performSegue(withIdentifier: "LoginToHome", sender: self)
+                }
+            }
+        }
     }
-    */
-
+    
 }
